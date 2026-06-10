a523a6b026f8c0341060282a83a93aaf1ad16108
MatinMorady <matin80000@gmail.com>
2026-06-10 07:26:34 +0330

    Create app/models.py

---
+++ app/models.py
@@ -0,0 +1,27 @@
+from datetime import datetime
+from flask_login import UserMixin
+from .extensions import db
+
+class User(UserMixin, db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    username = db.Column(db.String(80), unique=True)
+    password = db.Column(db.String(255))
+    is_admin = db.Column(db.Boolean, default=False)
+
+class Song(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    title = db.Column(db.String(200))
+    filename = db.Column(db.String(255))
+    views = db.Column(db.Integer, default=0)
+    created_at = db.Column(db.DateTime, default=datetime.utcnow)
+
+class Like(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    user_id = db.Column(db.Integer)
+    song_id = db.Column(db.Integer)
+
+class Comment(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    user_id = db.Column(db.Integer)
+    song_id = db.Column(db.Integer)
+    text = db.Column(db.String(300))
